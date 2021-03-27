93e603cd4827996ba43dae5ba58f472a9d70e4e4
ProgrammingPaddy <78576854+ProgrammingPaddy@users.noreply.github.com>
2021-03-27 14:02:41 -0400

    Update shareddefs.h

--- shareddefs.h
+++ shareddefs.h
@@ -39,10 +39,12 @@ struct Frame {
 
 
 
+typedef std::vector<Frame> FrameContainer;
+
 class Recorder {
 private:
 	bool is_recording_active = false;
-	
+	FrameContainer recording_frames;
 public:
 	void StartRecording() {
 		this->is_recording_active = true;
@@ -56,17 +58,20 @@ public:
 		return this->is_recording_active;
 	}
 
+	FrameContainer& GetActiveRecording() {
+		return this->recording_frames;
+	}
 };
 
 class Playback {
 private:
 	bool is_playback_active = false;
 	size_t current_frame = 0;
-	
+	FrameContainer& active_demo = FrameContainer();
 public:
-	void StartPlayback() {
+	void StartPlayback(FrameContainer& frames) {
 		this->is_playback_active = true;
-
+		this->active_demo = frames;
 	};
 
 	void StopPlayback() {
@@ -86,13 +91,13 @@ public:
 		this->current_frame = frame;
 	};
 
-
+	FrameContainer& GetActiveDemo() const {
+		return this->active_demo;
+	}
 };
 
 class RecordedFrames {
 public:
 	static Recorder recorder;
 	static Playback playback;
-	static std::vector<Frame> Frames;
 };
-
