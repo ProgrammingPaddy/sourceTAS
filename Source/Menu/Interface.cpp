2a205bdfe1d5885d185209c2ab9bc45c2b27cf45
ProgrammingPaddy <78576854+ProgrammingPaddy@users.noreply.github.com>
2021-03-27 14:02:04 -0400

    Update Interface.cpp

--- Source/Menu/Interface.cpp
+++ Source/Menu/Interface.cpp
@@ -3,25 +3,26 @@
 
 // Draw the user interface here!
 void BasehookInterface::OnEndScene() {
-	
+
 	if (!is_menu_visible)
 		return;
 	if (RecordedFrames::recorder.IsRecordingActive()) {
 		if (ImGui::Button("Stop Recording"))
 			RecordedFrames::recorder.StopRecording();
-	} else {
+	}
+	else {
 		if (ImGui::Button("Start Recording"))
 			RecordedFrames::recorder.StartRecording();
 
-		if (!RecordedFrames::Frames.empty()) {
+		if (!RecordedFrames::recorder.GetActiveRecording().empty()) {
 			if (ImGui::Button("Clear Recorded Frames"))
-				RecordedFrames::Frames.clear();
+				RecordedFrames::recorder.GetActiveRecording().clear();
 
 			if (RecordedFrames::playback.IsPlaybackActive() && ImGui::Button("Stop Playback"))
 				RecordedFrames::playback.StopPlayback();
 
 			if (!RecordedFrames::playback.IsPlaybackActive() && ImGui::Button("Start Playback"))
-				RecordedFrames::playback.StartPlayback();
+				RecordedFrames::playback.StartPlayback(RecordedFrames::recorder.GetActiveRecording());
 		}
 	}
 }
@@ -29,19 +30,19 @@ void BasehookInterface::OnEndScene() {
 // Check for key presses here in order to toggle menu visibility.
 bool BasehookInterface::OnInputMessage(UINT type, WPARAM w_param, LPARAM l_param) {
 	// Listen for INSERT key presses.
-	
+
 	if (type == WM_KEYUP && w_param == VK_INSERT) {
 		// Toggle menu visibility.
 		is_menu_visible = !is_menu_visible;
 
 		// Use the ImGui cursor.
 		ImGui::GetIO().MouseDrawCursor = is_menu_visible;
-		
+
 		// Enable or disable the in-game cursor to prevent issues.
 		matsurface->SetCursorAlwaysVisible(is_menu_visible);
 	}
 
-		// Pass input to ImGui if the menu is visible.
+	// Pass input to ImGui if the menu is visible.
 	if (is_menu_visible)
 		ImGui_ImplDX9_WndProcHandler(window, type, w_param, l_param);
 
