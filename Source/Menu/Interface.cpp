0fb30b87690fd588c5e0bca6a86e456105f45207
ProgrammingPaddy <78576854+ProgrammingPaddy@users.noreply.github.com>
2022-10-14 01:50:20 +0200

    Update Interface.cpp

--- Source/Menu/Interface.cpp
+++ Source/Menu/Interface.cpp
@@ -1,7 +1,6 @@
 #include "Interface.h"
 #include"../../shareddefs.h"
 
-// Draw the user interface here!
 void BasehookInterface::OnEndScene() {
 
 	if (!is_menu_visible)
@@ -33,25 +32,22 @@ void BasehookInterface::OnEndScene() {
 	}
 }
 
-// Check for key presses here in order to toggle menu visibility.
-bool BasehookInterface::OnInputMessage(UINT type, WPARAM w_param, LPARAM l_param) {
-	// Listen for INSERT key presses.
+
+bool BasehookInterface::OnInputMessage(UINT type, WPARAM w_param, LPARAM l_param) {	
 
 	if (type == WM_KEYUP && w_param == VK_INSERT) {
-		// Toggle menu visibility.
+		
 		is_menu_visible = !is_menu_visible;
 
-		// Use the ImGui cursor.
+		
 		ImGui::GetIO().MouseDrawCursor = is_menu_visible;
 
-		// Enable or disable the in-game cursor to prevent issues.
+		
 		matsurface->SetCursorAlwaysVisible(is_menu_visible);
 	}
 
-	// Pass input to ImGui if the menu is visible.
 	if (is_menu_visible)
 		ImGui_ImplDX9_WndProcHandler(window, type, w_param, l_param);
 
-	// Pass input to game if the menu is not visible.
 	return !is_menu_visible;
 }
